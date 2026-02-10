7fa62094d60f22145e1c85269aed52852e2722ac
Nisa Nur Işık <144225424+nisanurisik@users.noreply.github.com>
2026-02-10 11:06:27 +0300

    Update OdemeTuru.cs

--- Models/OdemeTuru.cs
+++ Models/OdemeTuru.cs
@@ -6,11 +6,18 @@ enum OdemeTuru
     Yapilan
 }
 
+enum TahsilatTuru
+{
+    Nakit,
+    Kart
+}
+
 class Odeme
 {
     public int Id;
     public int CariId;
     public string CariAdi;
     public OdemeTuru Tur;
+    public TahsilatTuru Tahsilat;
     public decimal Tutar;
 }
